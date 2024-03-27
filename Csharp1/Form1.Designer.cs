8d717136167ac6a0db5aa3b2332e87c7dc56cc8e
Anderson <aorozom@unicesar.edu.co>
2024-03-27 18:11:47 -0500

    Agregar archivo de Curso CSharp

---
+++ Csharp1/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Csharp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Ok = new System.Windows.Forms.Button();
+            this.btn_Close = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_Nombre = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_Ok
+            // 
+            this.btn_Ok.Location = new System.Drawing.Point(338, 302);
+            this.btn_Ok.Name = "btn_Ok";
+            this.btn_Ok.Size = new System.Drawing.Size(139, 57);
+            this.btn_Ok.TabIndex = 0;
+            this.btn_Ok.Text = "OK";
+            this.btn_Ok.UseVisualStyleBackColor = true;
+            // 
+            // btn_Close
+            // 
+            this.btn_Close.Location = new System.Drawing.Point(520, 302);
+            this.btn_Close.Name = "btn_Close";
+            this.btn_Close.Size = new System.Drawing.Size(132, 57);
+            this.btn_Close.TabIndex = 1;
+            this.btn_Close.Text = "CLOSE";
+            this.btn_Close.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(349, 194);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Bienvenido:";
+            // 
+            // txt_Nombre
+            // 
+            this.txt_Nombre.Location = new System.Drawing.Point(418, 191);
+            this.txt_Nombre.Name = "txt_Nombre";
+            this.txt_Nombre.Size = new System.Drawing.Size(198, 20);
+            this.txt_Nombre.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txt_Nombre);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_Close);
+            this.Controls.Add(this.btn_Ok);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Ok;
+        private System.Windows.Forms.Button btn_Close;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_Nombre;
+    }
+}
+
---
+++ Csharp1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Csharp1
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        /// 
+        [STAThread]
+        static void Main()
+        {
+           // string Nombre = "Anderson";
+
+           // int num1 = 450;
+           // uint num2 = 500; //uint nunca valor negativo
+           // float num3 = 99.3f;// f indica numero flotante (mas precision decimal)
+           // double num4 = 22.43;// nuemro decimal mas grande que float
+           // decimal num5 = 7983456.974323m;//m indica decimal (calculos gigantes)
+           // byte num6 = 255;//maximo 255
+            
+           //// MessageBox.Show(Nombre);
+           // //MessageBox.Show(num3.ToString());//Conversion
+
+           // bool acceso = true;
+           //// MessageBox.Show(acceso.ToString());
+
+           // DateTime fecha = DateTime.Now;
+           // //MessageBox.Show(fecha.ToShortDateString().ToString());
+
+           // //int num7 = 43;
+           // //int num8 = 50;
+           // //int num9 = 100;
+           // //int num10 = 200;
+           // //int num12 = 500;mucho espacio vertical para crear variables
+
+           // int num7, num8, num9, num10, num11;
+           // num7 = 39; num8 = 33; num9 = 213; num10 = 100; num11 = 104;
+
+           // const string dato = "Hola puebas constante";
+           // //dato = "cambio constante";
+           // MessageBox.Show(dato);
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
