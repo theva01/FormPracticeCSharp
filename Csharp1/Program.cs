235a8a54030ba8b6d34a7b41da54a7bfaedd5e88
theva01 <Oeduardo263@gmail.com>
2024-04-04 00:07:52 -0500

    Uso basico de vectores

--- Csharp1/Program.cs
+++ Csharp1/Program.cs
@@ -15,17 +15,19 @@ namespace Csharp1
         [STAThread]
         static void Main()
         {
-            
-            string varPrueba = "";
-            int x = 0;
-            
-            do {
-                varPrueba += x.ToString() + "-";
 
-                x++;
-            } while ( x > 10 );
-            MessageBox.Show( varPrueba );
+            int[] numeros = { 1, 2, };
+            int[] numeros2 = new int[7];
 
+            numeros2[0] = 1;
+            numeros2[1] = 233;
+            numeros2[2] = 334;
+            numeros2[3] = 0;
+            numeros2[4] = 443;
+            numeros2[5] = 0;
+            numeros2[6] = 436;
+
+            MessageBox.Show(numeros2[4].ToString());
             //Application.EnableVisualStyles();
             //Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new Form1());
