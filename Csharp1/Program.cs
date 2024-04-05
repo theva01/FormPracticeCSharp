35b0521e52c2bb7c3bff8c3cf3e5f3534b5cb413
theva01 <Oeduardo263@gmail.com>
2024-04-05 14:29:14 -0500

    Uso for each

--- Csharp1/Program.cs
+++ Csharp1/Program.cs
@@ -16,44 +16,12 @@ namespace Csharp1
         static void Main()
         {
 
-            int[] numeros = { 1, 2, };
-            int[] numeros2 = new int[7];
 
-            numeros2[0] = 1;
-            numeros2[1] = 233;
-            numeros2[2] = 334;
-            numeros2[3] = 0;
-            numeros2[4] = 443;
-            numeros2[5] = 0;
-            numeros2[6] = 436;
+            int[] numeros2 = { 3, 44, 6 };
 
-
-
-            //for (int i = 0; i < numeros2.Length; i++)
-            //{
-            //    MessageBox.Show(numeros2[i].ToString());
-            //}
-
-            int[,] matriz = new int[5,5];
-
-            for(int i = 0; i < matriz.GetLength(0); i++)//i filas
+            foreach (var datoNum in numeros2)
             {
-                for(int j = 0; j < matriz.GetLength(1); j++)//j columnas
-                {
-
-                    matriz[i, j] = 10 * (i+1)*(j+1);
-
-                }
-            }
-
-            for (int i = 0; i < matriz.GetLength(0); i++)//i filas
-            {
-                for (int j = 0; j < matriz.GetLength(1); j++)//j columnas
-                {
-
-                    MessageBox.Show("La posicion es: " + i + "," + j + "=" + matriz[i, j].ToString());
-
-                }
+                MessageBox.Show(datoNum.ToString());
             }
 
             //Application.EnableVisualStyles();
